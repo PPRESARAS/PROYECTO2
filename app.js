968adaa7c63e7c9b63c8ac5096b1b060ddb9e137
Pedro <ppresaras@udesa.edu.ar>
2020-05-24 11:45:44 -0300

    mas sequelize

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var db = require ('../PROYECTO2/database/models')
 
 var app = express();
 
