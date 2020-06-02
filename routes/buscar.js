9bba85b86b6dadbd52808870aaf1bb160460b152
Pedro <ppresaras@udesa.edu.ar>
2020-06-01 23:53:27 -0300

    algo

--- routes/buscar.js
+++ routes/buscar.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('buscar', { title: 'Express' });
+  res.render('buscador', { title: 'Express' });
 });
 
 module.exports = router;
