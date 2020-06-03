8e8d1d646f749ce113c4e734197197d03a7c47ac
Pedro <ppresaras@udesa.edu.ar>
2020-06-03 20:06:13 -0300

    bycrptjs

--- routes/login.js
+++ routes/login.js
@@ -9,17 +9,12 @@ const controller = require('../controllers/loginController');
 
 router.get('/', controller.confirmUser);
 
-<<<<<<< HEAD
-<<<<<<< 6cbe63bf59ee165781078d7a714728a9de7b2024
-router.post('/reviews', usersController.confrimUser);
-=======
-router.post('/', controller.logUser);
->>>>>>> correcciones
-=======
-router.post('/', controller.logUser);
->>>>>>> master
 
-router.get('/reviews/:id', usersController.get.Reviews);
+// router.post('/reviews', controller.confirmUser);
+
+router.post('/', controller.logUser);
+
+// router.get('/reviews/:id', controller.get.reviews);
 
 
 module.exports = router;
